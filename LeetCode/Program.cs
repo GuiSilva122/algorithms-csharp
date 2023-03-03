22b436d32f036d936af9834bd530dc98edcc1b35
Guilherme De Souza Silva <guilherme.silva@authoritypartners.com>
2023-03-03 13:11:03 -0300

    Leet Code.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,3 @@
+﻿using LeetCode;
+
+RomanToInt.TestSolution();
---
+++ LeetCode/RomanToInt.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode
+{
+    internal class RomanToInt
+    {
+        private static readonly Dictionary<char, int> RomansMap = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public static int Convert(string romanNumber)
+        {
+            int converted = 0;
+
+            for (int i = 0; i < romanNumber.Length; i++)
+            {
+                int currentValue = RomansMap[romanNumber[i]];
+                int nextValue = 0;
+
+                if (i + 1 < romanNumber.Length)
+                    nextValue = RomansMap[romanNumber[i + 1]];
+
+                if (currentValue < nextValue)
+                {
+                    converted += (nextValue - currentValue);
+                    i++;
+                }
+                else
+                    converted += currentValue;
+            }
+            return converted;
+        }
+
+        public static void TestSolution()
+        {
+            var result = Convert("III"); //3
+            result = Convert("LVIII"); //58
+            result = Convert("MCMXCIV"); //1994
+        }
+    }
+}
