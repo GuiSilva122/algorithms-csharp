fb6bf5629286b95f3946ba5eb24aeb7c3e45bf2d
Guilherme de Souza Silva <gui.iug.net@gmail.com>
2023-04-25 07:56:14 -0300

    Leet Code Day 8 of 75

---
+++ LeetCode/75/8_Tree_LowestCommonAncestorBST.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode._75
+{
+    public class _8_Tree_LowestCommonAncestorBST
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root.val < p.val && root.val < q.val)
+                return LowestCommonAncestor(root.right, p, q);
+            else if (root.val > p.val && root.val > q.val)
+                return LowestCommonAncestor(root.left, p, q);
+            return root;
+        }
+
+        public TreeNode LowestCommonAncestorV2(TreeNode root, TreeNode p, TreeNode q)
+        {
+            var node = root;
+            while (node != null)
+            {
+                if (node.val < p.val && node.val < q.val)
+                    node = node.right;
+                else if (node.val > p.val && node.val > q.val)
+                    node = node.left;
+                else
+                    return node;
+            }
+            return node;
+        }
+    }
+}
---
+++ LeetCode/75/8_Tree_ValidateBST.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode._75
+{
+    public class _8_Tree_ValidateBST
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public bool IsValidBSTV1(TreeNode root)
+            {
+                return IsValidBSTRecursion(root, null);
+            }
+
+            public bool IsValidBSTRecursion(TreeNode root, int? prev)
+            {
+                if (root == null) 
+                    return true;
+
+                if (!IsValidBSTRecursion(root.left, null))
+                    return false;
+
+                if (prev.HasValue && root.val <= prev.Value) 
+                    return false;
+                
+                prev = root.val;
+                return IsValidBSTRecursion(root.right, prev);
+            }
+
+            public bool IsValidBSTV2(TreeNode root)
+            {
+                if (root == null) 
+                    return true;
+
+                int? prev = null;
+                var stack = new Stack<TreeNode>();
+                while (stack.Count > 0 || root != null)
+                {
+                    while (root != null)
+                    {
+                        stack.Push(root);
+                        root = root.left;
+                    }
+                    root = stack.Pop();
+                    if (prev.HasValue && root.val <= prev.Value)
+                        return false;
+                    prev = root.val;
+                    root = root.right;
+                }
+                return true;
+            }
+        }
+    }
+}
