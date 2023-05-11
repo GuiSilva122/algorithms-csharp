e0b6e001d039f28911ccc9b642d5a0e0733661c3
Guilherme De Souza Silva <guilherme.silva@authoritypartners.com>
2023-05-11 11:14:42 -0300

    Refactoring unused testing cases

--- LeetCode/75/8_Tree_ValidateBST.cs
+++ LeetCode/75/8_Tree_ValidateBST.cs
@@ -64,14 +64,5 @@
             var root = new TreeNode(4, left, right);
             var result = IsValidBSTV2(root);
         }
-
-        private bool IsPangram(string sentence)
-        {
-            int[] frequencyCount = new int[26];
-            foreach (char c in sentence)
-                frequencyCount[c - 'a']++;
-
-            foreach (int count)
-        }
     }
 }
